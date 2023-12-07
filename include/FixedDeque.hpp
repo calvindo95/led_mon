692639ca1cddcec6cffbd33496597be56f26ae0f
calvindo95 <calvindo95@gmail.com>
2023-12-06 20:43:22 -0500

    Added inital cpu monitoring

---
+++ include/FixedDeque.hpp
@@ -0,0 +1,16 @@
+#include <deque>
+
+template <typename T, int MaxLen>
+class FixedDeque: public std::deque<T>{
+    public:
+        void push_back(T item){
+            if(this->size() >= MaxLen){
+                this->pop_front();
+            }
+            std::deque<T>::push_back(item);
+        }
+
+        T get_avg(){
+            return (std::accumulate(std::deque<T>::begin(), std::deque<T>::end(), 1.0) / std::deque<T>::size());
+        }
+};
