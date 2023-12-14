b5f940122cfa47c603fda1319076b28d39f9eaae
calvindo95 <calvindo95@gmail.com>
2023-12-13 23:21:52 -0500

    Testing performance

--- source/main.cpp
+++ source/main.cpp
@@ -26,9 +26,10 @@ int main()
         }
     }
     else{
+        double cpu_val;
         while(true){
             std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
-            double cpu_val = cpu.get_cpu();
+            cpu_val = cpu.get_cpu();
             std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
             std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::nanoseconds> (end - begin).count() << "[ns]" << std::endl;
             PostJson pj(cpu_val);
