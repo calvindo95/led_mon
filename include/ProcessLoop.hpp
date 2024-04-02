8e050f71588739e71c680c3a57f1c9d2b2129794
calvindo95 <calvindo95@gmail.com>
2024-04-01 22:10:51 -0400

    Added ProcessLoop class to cleanup main.cpp

---
+++ include/ProcessLoop.hpp
@@ -0,0 +1,29 @@
+#include <PostJson.hpp> 
+#include <CPU.hpp>
+
+#include <json.hpp>
+#include <iostream>
+
+class ProcessLoop{
+    private:
+        bool m_cpu_mon = false;
+        bool m_mem_mon = false;
+
+        Config& m_config = Config::get_config();
+        
+        bool tmp_bool;
+        CPU m_cpu;
+
+        void process();
+
+    public:
+        ProcessLoop();
+
+        void start();
+
+        void enable_cpu_mon();
+        void disable_cpu_mon();
+
+        void enable_mem_mon();
+        void disable_mem_mon();
+};
---
+++ source/ProcessLoop.cpp
@@ -0,0 +1,52 @@
+#include <ProcessLoop.hpp>
+
+ProcessLoop::ProcessLoop(){
+    // init
+}
+
+void ProcessLoop::process(){
+    // logic to retrieve hardware data and post json
+    while(true){
+        PostJson pj;
+
+        if(m_cpu_mon){
+            json j_cpu;
+            
+            if(m_cpu.is_multi_cpu()){
+                j_cpu.update(m_cpu.get_json(m_cpu.calc_multi_cpu()));
+            }
+            else{
+                j_cpu.update(m_cpu.get_json(m_cpu.calc_cpu()));
+            }
+            pj.update_json(j_cpu);
+        }
+
+        if(m_mem_mon){
+            // Need to implement memory monitoring
+        }
+
+        pj.post_json();
+        usleep(std::stoi(m_config.get_env_var("uSECONDS")));
+    }
+}
+
+void ProcessLoop::start(){
+    process();
+}
+
+void ProcessLoop::enable_cpu_mon(){
+    m_cpu_mon = true;
+}
+
+void ProcessLoop::disable_cpu_mon(){
+    m_cpu_mon = false;
+}
+
+void ProcessLoop::enable_mem_mon(){
+    m_mem_mon = true;
+}
+
+void ProcessLoop::disable_mem_mon(){
+    m_mem_mon = false;
+}
+    
